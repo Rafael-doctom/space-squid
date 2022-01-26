26210a9f713fc1ca88b29bcd60863796c34d2543
Augusto Ravazoli <ravazoli.augusto@gmail.com>
2022-01-26 17:23:30 -0300

    better Text.js

--- src/core/components/Text.js
+++ src/core/components/Text.js
@@ -1,19 +1,15 @@
-function Text(size = "16px", color = "black", position = "black", gameArea) {
-  this.size = size;
-  this.color = color;
-  this.position = position;
-
-  this.fontFace = new FontFace("Arcade Classic", "url(../src/assets/font/ArcadeClassic.ttf)");
+function Text(gameArea) {
+  this.fontFace = new FontFace("Retro Gaming", "url(../src/assets/font/RetroGaming.ttf)");
   this.ctx = gameArea.canvas.getContext("2d");
   this.fontFace.load().then(font => {
     document.fonts.add(font);
-    this.ctx.font = `16px Arcade Classic`;
+    this.ctx.font = `6px Retro Gaming`;
   })
 
-  this.write = function(string, x, y) {
-    this.ctx.font = `${this.size} Arcade Classic`;
-    this.ctx.textAlign = this.position;
-    this.ctx.fillStyle = this.color;
+  this.write = function(string, x, y, size, color, position) {
+    this.ctx.font = `${size} Retro Gaming`;
+    this.ctx.textAlign = position;
+    this.ctx.fillStyle = color;
     this.ctx.fillText(string, x, y);
   };
 }
