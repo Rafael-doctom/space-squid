fc0f612fc81fb99203bf2c2dba9ebc0dc2bfd9f7
Augusto Ravazoli <ravazoli.augusto@gmail.com>
2022-01-26 17:24:08 -0300

    game logic moved to UI

--- src/index.js
+++ src/index.js
@@ -3,19 +3,15 @@ import Core from "./core/Core.js";
 import UI from "./UI/UI.js";
 import Level0 from "./game/levels/level0.js";
 
-import xor from "./utils/xor.js";
 
 const levels = [
   new Level0()
 ];
 
-let state = 0;
-
 window.addEventListener("click", () => {
-  if (state > 0)
+  if (document.fullscreenElement && document.fullscreenElement.nodeName == 'CANVAS') 
     return;
 
-  state++;
   startGame();
 });
 
@@ -26,25 +22,8 @@ function startGame() {
   updateGameArea();
 }
 
-async function updateGameArea() {
-  if (UI.title.cursor.choice == "none") {
-    UI.title.render();
-    UI.title.music.play();
-    //UI.credits.render();
-  }
-  
-  if (UI.title.cursor.choice == "start") {
-    if (!xor(Core.KeyboardControl.keysPressed.enterPressed == 1, Core.TouchControl.buttonsPressed.enter == 1))  
-      levels[0].start();
-
-    if (levels[0].loosed) {
-      UI.gameOver.render();
-      if (UI.gameOver.cursor.choice == "restart") {
-        levels[0] = new Level0();
-      }
-    }
-  }
-  
-  Core.TouchControl.renderButtons();
+function updateGameArea() {
+  UI.transition(levels[0]);
+  UI.render(levels[0]);
   requestAnimationFrame(updateGameArea);
 }
