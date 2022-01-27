6717152b8b0d3b48f7cc5495b8d7f723f4ca218e
Augusto Ravazoli <ravazoli.augusto@gmail.com>
2022-01-27 13:19:20 -0300

    attempt 1 to fix the path of the images

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/script.js',
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: './src/assets/favicon.png',
+      favicon: 'src/assets/favicon.png',
       title: 'Space Squid',
-      template: './src/index.html'
+      template: 'src/index.html'
     }),
   ],
   output: {
     filename: 'bundle.js',
+    publicPath: this.mode === 'production' ? './space-squid' : './',
     path: path.resolve(__dirname, 'docs'),
     clean: true
   },
