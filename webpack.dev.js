28bf3c6ecc3c4cf64abc862491270228b841a225
Augusto Ravazoli <ravazoli.augusto@gmail.com>
2022-01-28 22:17:51 -0300

    v0.1.1

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     static: {
-      directory: './dist',
+      directory: './docs',
     },
     compress: true,
     port: 3000,
